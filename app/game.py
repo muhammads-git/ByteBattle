ed77151467a5ffa1186914dce212b2f504bc198f
muhammads-git <roadprogrammar@gmail.com>
2026-09-17 13:15:12 +0500

    changes made

--- app/game.py
+++ app/game.py
@@ -7,17 +7,33 @@ right_ans = [8,20,1]
 options = [(1,3,4,5),(1,5,7,20),(4,5,3,1)]
 
 ## calcualte score...
-def calculatScore(time_taken, ans: bool):
-   pass
+def calculateScore(time_taken, correct: bool):
+   """   
+      calculate score per timetaken...
+      Forumula;
+      slope = the change in points from point a to b i.e points from 1s to 2s.
+      score = slope * (time_taken - max_time) + max_score
+   """
+   max_time = 15
+   min_score = 2
+   formula = -3 * (time_taken - max_time) + min_score
 
-
-
-
-
+   score = 0
+   if correct:
+      if time_taken <= 15:
+         score = formula
+         return score
+      else:
+         return 0
+   else:
+      return 0
+   
+   
+      
 
 
 while True:
-   
+   totla_score = 0
    print('game starts')
    for index,ques in enumerate(quiz):
       start_time = time.time()
@@ -27,11 +43,20 @@ while True:
       ans = int(input())
       end_time = time.time()
       # calculate the ans time... 
-      time_took = end_time - start_time
-      print(f'TIme taken: {time_took}')
+      float_t = end_time - start_time
+      time_took = int(float_t)
+      # if time taken is less than 1
+      if time_took < 1:
+         time_took = 1
+      
+      print(f'Time taken: {time_took}')
       if ans == right_ans[index]:
-         pass
-         # calculate score according to time
-   # break
+         score = calculateScore(time_took,True)
+         totla_score += score
+      else:
+         score = calculateScore(time_took,False)
+         totla_score += score
 
+   print(totla_score)
+   break
 
