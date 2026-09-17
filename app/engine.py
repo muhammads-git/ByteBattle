641c0b486de16ba9370dd8e3428e8030100cbba8
muhammads-git <roadprogrammar@gmail.com>
2026-09-17 20:52:46 +0500

    tests/

--- app/engine.py
+++ app/engine.py
@@ -8,18 +8,21 @@ def calculateScore(time_taken, correct: bool):
       calculate score per timetaken...
       Forumula;
       slope = the change in points from point a to b i.e points from 1s to 2s.
-      score = slope * (time_taken - max_time) + max_score
+      score = slope * (time_taken - max_time) + min_time
+      (x1 - y1) / (x2 - y2) 
    """
    max_time = 15
    min_score = 2
-   formula = -3.4285714 * (time_taken - max_time) + min_score
-
+  
    score = 0
    if correct:
-      if time_taken < 1:
+      if time_taken > 0 and time_taken < 1:
          time_taken = 1
       if time_taken <= max_time:
+         # formulaaa to compute
+         formula = -3.4285714 * (time_taken - max_time) + min_score
          score = formula
+         # round off
          return round(score)
       else:
          return 0
--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,5 +1,37 @@
 from app.engine import calculateScore
 
-def test_engine():
-   result = calculateScore(1,True)
+def test_ali1s():
+   result1 = calculateScore(1,True)
+   assert result1 == 50
+def test_usman2s():
+   result2 = calculateScore(2,True)
+   assert result2 == 47
+
+def test_dani3s():
+   result3 = calculateScore(3,True)
+   assert result3 == 43
+def test_hammad16s():
+   result = calculateScore(16,True)
+   assert result == 0
+
+def test_hina12s():
+   result = calculateScore(12,True)
+   assert result == 12
+
+def test_boundary_15s():
+   result= calculateScore(15,True)
+   assert result == 2
+# correct = False
+def test_dani1s():
+   result = calculateScore(1,False)
+   assert result == 0
+
+
+   # test < 1
+def test_lessthan_1():
+   result = calculateScore(0.2,True)
+   assert result == 50
+
+def test_negative_time():
+   result = calculateScore(-1,True)
    assert result == 50
