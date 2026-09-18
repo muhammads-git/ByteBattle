def51f75455b74e18c8996a84b9d6ae91b86af30
muhammads-git <roadprogrammar@gmail.com>
2026-09-18 14:21:03 +0500

    setup:postgresql/sqlachemy/alembic

---
+++ app/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import sessionmaker,declarative_base
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# CREATE CONNECTION WITH DB...
+DATABASE_URL= os.getenv('DATABASE_URL')
+engine = create_engine(DATABASE_URL)
+
+# create sessions
+SessionLocal = sessionmaker(autoflush=False,autocommit=False,bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+   db = SessionLocal()
+   try:
+      yield db
+   finally:
+      db.close()
---
+++ app/models.py
@@ -0,0 +1,13 @@
+from app.database import Base
+
+
+
+class Room(Base):
+   __tablename__ = 'rooms'
+
+
+
+class Player(Base):
+   __tablename__ = 'players'
+
+
