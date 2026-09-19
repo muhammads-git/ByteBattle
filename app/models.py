8f8357735f5b28c6396ad8239df07f7960ac9b88
muhammads-git <roadprogrammar@gmail.com>
2026-09-19 13:50:50 +0500

    models.py, changes made

--- app/models.py
+++ app/models.py
@@ -1,25 +1,35 @@
 from app.database import Base
 from sqlalchemy import Column,Integer,String,ForeignKey,Text,DateTime
 from datetime import datetime, timedelta,timezone
+from sqlalchemy.dialects.postgresql import ARRAY
 
 class Room(Base):
    __tablename__ = 'rooms'
-   id = Column(Integer, primary_key=True,autoincrement=True)
-   host_id = Column(Integer,nullable=False)
-   room_code = Column(String(255),nullable=False,unique=True)
-   room_state = Column(String,default='waiting')
-   created_at = Column(DateTime)
-   room_started_at = Column()
-   expires_at = Column(DateTime)
-   current_question_started_at = Column()
-   current_question_index = Column(Integer,default=0)
-
-
+   id = Column(Integer, primary_key=True, autoincrement=True)
+   host_id = Column(Integer, ForeignKey('players.id'), nullable=False)
+   room_code = Column(String(255), nullable=False, unique=True)
+   room_state = Column(String, default='waiting')
+   created_at = Column(DateTime, default=datetime.utcnow)
+   room_started_at = Column(DateTime)  # null until host clicks start
+   expires_at = Column(DateTime, default=lambda: datetime.utcnow() + timedelta(minutes=10))
+   current_question_started_at = Column(DateTime)
+   current_question_index = Column(Integer, default=0)
 
 
 class Player(Base):
    __tablename__ = 'players'
+   id = Column(Integer,primary_key=True,autoincrement=True)
+
+
+
 
 class RoomPlayer(Base):
    __tablename__ = 'room_players'
 
+class Question(Base):
+   __tablename__ = 'questions'
+   id = Column(Integer, primary_key=True,autoincrement=True)
+   question = Column(String(255),nullable=False) # store the question as strings...
+   options = Column(ARRAY(String),nullable=False)
+   correct_option = Column(Integer,nullable=False)
+   
