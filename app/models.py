ba46afddfdbabacae0e62ca2bf52b2465fd47c1e
muhammads-git <roadprogrammar@gmail.com>
2026-09-19 11:28:16 +0500

    schema

--- app/models.py
+++ app/models.py
@@ -1,10 +1,19 @@
 from app.database import Base
-
-
+from sqlalchemy import Column,Integer,String,ForeignKey,Text,DateTime
+from datetime import datetime, timedelta,timezone
 
 class Room(Base):
    __tablename__ = 'rooms'
-   
+   id = Column(Integer, primary_key=True,autoincrement=True)
+   host_id = Column(Integer,nullable=False)
+   room_code = Column(String(255),nullable=False)
+   room_state = Column(String,default='')
+   created_at = Column(DateTime)
+   expires_at = Column(DateTime)
+   current_question_index = Column()
+   current_question_started_at = Column()
+   room_started_at = Column()
+
 
 
 
