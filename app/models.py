3771e2ba9eb5736eb52b3f9fbe2352891e6338c4
muhammads-git <roadprogrammar@gmail.com>
2026-09-20 11:40:22 +0500

    schema.design

--- app/models.py
+++ app/models.py
@@ -3,11 +3,13 @@ from sqlalchemy import Column,Integer,String,ForeignKey,Text,DateTime
 from datetime import datetime, timedelta,timezone
 from sqlalchemy.dialects.postgresql import ARRAY
 
+
 class Room(Base):
    __tablename__ = 'rooms'
    id = Column(Integer, primary_key=True, autoincrement=True)
    host_id = Column(Integer, ForeignKey('players.id'), nullable=False)
    room_code = Column(String(255), nullable=False, unique=True)
+   room_quizes = Column(ARRAY(Integer),nullable=False)  # every room has its own assigned 3 quizes..
    room_state = Column(String, default='waiting')
    created_at = Column(DateTime, default=datetime.utcnow)
    room_started_at = Column(DateTime)  # null until host clicks start
@@ -19,17 +21,25 @@ class Room(Base):
 class Player(Base):
    __tablename__ = 'players'
    id = Column(Integer,primary_key=True,autoincrement=True)
+   player_name = Column(String(255),nullable=False,unique=True)
+   roll_no = Column(Integer,unique=True,nullable=False)
+   password_hash = Column(String(255),nullable=False)
 
 
 
 
 class RoomPlayer(Base):
    __tablename__ = 'room_players'
+   id = Column(Integer,primary_key=True,autoincrement=True)
+   room_id = Column(Integer,ForeignKey('rooms.id'),nullable=False)
+   player_id = Column(Integer,ForeignKey('players.id'),nullable=False)
+   score = Column(Integer,default=0,nullable=False)
+   joined_at = Column(DateTime,default=datetime.utcnow)
+
 
 class Question(Base):
    __tablename__ = 'questions'
    id = Column(Integer, primary_key=True,autoincrement=True)
-   question = Column(String(255),nullable=False) # store the question as strings...
+   question = Column(Text,nullable=False) # store the question as strings...
    options = Column(ARRAY(String),nullable=False)
    correct_option = Column(Integer,nullable=False)
-   
