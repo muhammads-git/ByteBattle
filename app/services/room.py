ae267c4b39f4ea2da4564bfd4545e2a0137f5464
muhammads-git <roadprogrammar@gmail.com>
2026-09-21 18:23:46 +0500

    services dummy

---
+++ app/services/room.py
@@ -0,0 +1,13 @@
+from app.models import Room,RoomPlayer,Player,Question
+
+
+
+def create_room():
+   """
+   create_room, allows a user/player to create_room..
+   : insertion in db
+   a: generate room_code unique
+   b: attach that to url/unique_code
+   c: player copies and shares, the link....
+   """
+   pass
