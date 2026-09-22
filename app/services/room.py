8d30be5fbbf942488f93380481ec1ddd7820fb8c
muhammads-git <roadprogrammar@gmail.com>
2026-09-22 11:46:02 +0500

    changes made

--- app/services/room.py
+++ app/services/room.py
@@ -1,6 +1,7 @@
 from app.models import Room,RoomPlayer,Player,Question
 from app.database import get_db
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 import uuid
 import random
 import string
@@ -30,8 +31,13 @@ def create_room(player_id : str):
    question_count = db.query(Question).count()
    random_questions = random.sample(range(1, question_count + 1), k=3) 
    questions = db.query(Question.id).filter(Question.id.in_(random_questions)).all()
-   if not questions:
-      print('No question found.')
+   # [(),(),()] add integers from tuples to a list 
+   list_quest = []
+   for question in questions:
+      list_quest.append(question[0])
+
+   if not list_quest or len(list_quest) < 3:
+      raise ValueError(f'Not enough questions in the database to create a room.')
 
    # insert into db 
    while True:
@@ -39,24 +45,27 @@ def create_room(player_id : str):
          new_room = Room(
             room_code = code,
             host_id = player_id,
-            room_quizes=questions # see if the syntax is right as this is: array(int)
+            room_quizes=list_quest # see if the syntax is right as this is: array(int)
          )
-
+         # add and commit() //
          db.add(new_room)
          db.commit()
-         
-      except Exception as e:
-         print('Code exists, re-generate.')
-         # again call the function
-         code = generate_unique_room_code()
+         break # succes break the loop
+      except IntegrityError as e:
+         db.rollback() # 
+         error_msg = str(e.orig).lower()
+
+         # Case A: IF unique in erros means, its duplication so , retry again...
+         if 'unique' in error_msg or 'duplicate' in error_msg:
+            code = generate_unique_room_code() # re-generate the code and continue to while loop
+            continue # retry, re-generate the code and insert
+         # Case B: IF its foreign key or Id error, stop .
+         elif 'foreign key' in error_msg or 'violates fk' in error_msg:
+            raise ValueError(f"Cannot create room: Host Player ID '{player_id}' does not exist.")
+
+         else:
+            raise e
    
-
-
-   pass
-
-
-
-
 
 def join_room():
    pass
