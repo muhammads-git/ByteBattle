d8c86b0abb361a02c5a07f4288a93aecebad5d09
muhammads-git <roadprogrammar@gmail.com>
2026-09-25 12:23:49 +0500

    changes made

--- app/jobs/room_poller.py
+++ app/jobs/room_poller.py
@@ -1,17 +1,25 @@
 from app.database import get_db
 from app.models import *
+from datetime import datetime
 
 
-
-def fetch_in_progress_rooms():
+def advance_rooms_questions():
    db = next(get_db)
 
-   # fetch the in_progress rooms
+   """
+   fetch the in_progress rooms   
+   """
    rooms = db.query(Room).filter(Room.room_state == 'in_progress').all()
    if not rooms:
       raise ValueError(f'No in_progress rooms found.')
 
-   
-
-   return rooms
-
+   for room in rooms:
+      # current_question_started_time = room.current_question_started_at
+      # calculate the time if >< to 15
+      # act accordinlgly   
+      time = datetime.utcnow() - room.current_question_started_at.total_seconds()
+      if time >= 15:
+         # check if the question is last or last index
+         last_index = room.current_question_index 
+         if last_index == len(room.room_quizes) - 1:
+            pass
