21177043d673ce37d832ae3b0d383be1d4ab6b21
muhammads-git <roadprogrammar@gmail.com>
2026-09-25 12:55:29 +0500

    changes made

--- app/jobs/room_poller.py
+++ app/jobs/room_poller.py
@@ -4,22 +4,34 @@ from datetime import datetime
 
 
 def advance_rooms_questions():
-   db = next(get_db)
+   db = next(get_db())
 
    """
-   fetch the in_progress rooms   
+   fetch the in_progress rooms 
+   returns None  
    """
    rooms = db.query(Room).filter(Room.room_state == 'in_progress').all()
-   if not rooms:
-      raise ValueError(f'No in_progress rooms found.')
 
    for room in rooms:
       # current_question_started_time = room.current_question_started_at
       # calculate the time if >< to 15
       # act accordinlgly   
-      time = datetime.utcnow() - room.current_question_started_at.total_seconds()
-      if time >= 15:
+      time = datetime.utcnow() - room.current_question_started_at
+      if time.total_seconds() >= 15:
          # check if the question is last or last index
-         last_index = room.current_question_index 
-         if last_index == len(room.room_quizes) - 1:
-            pass
+         total_question_index = len(room.room_quizes) - 1
+         if room.current_question_index == total_question_index:
+            # Last question
+            """ change the question state to ended as the question ended...""" 
+            room.room_state = 'ended'
+         else:
+            # increment the curr ques index
+            room.current_question_index += 1
+            # reset the curr ques timer to now
+            room.current_question_started_at = datetime.utcnow()
+   # commit the changes 
+   db.commit()
+
+   # returns nothing
+
+
